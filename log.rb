9103028eaad326fed77cea690299efddedbc5584
Yusuke Anzai <qowgahoh@gmail.com>
2018-08-27 17:39:32 +0900

    Create log file of automatic process

---
+++ log.rb
@@ -0,0 +1,88 @@
+Cookie#domain returns dot-less domain name now. Use Cookie#dot_domain if you need "." at the beginning.
+success
+Cookie#domain returns dot-less domain name now. Use Cookie#dot_domain if you need "." at the beginning.
+success
+Cookie#domain returns dot-less domain name now. Use Cookie#dot_domain if you need "." at the beginning.
+success
+Cookie#domain returns dot-less domain name now. Use Cookie#dot_domain if you need "." at the beginning.
+success
+Cookie#domain returns dot-less domain name now. Use Cookie#dot_domain if you need "." at the beginning.
+success
+Cookie#domain returns dot-less domain name now. Use Cookie#dot_domain if you need "." at the beginning.
+success
+Cookie#domain returns dot-less domain name now. Use Cookie#dot_domain if you need "." at the beginning.
+success
+Cookie#domain returns dot-less domain name now. Use Cookie#dot_domain if you need "." at the beginning.
+success
+Cookie#domain returns dot-less domain name now. Use Cookie#dot_domain if you need "." at the beginning.
+success
+Cookie#domain returns dot-less domain name now. Use Cookie#dot_domain if you need "." at the beginning.
+success
+Cookie#domain returns dot-less domain name now. Use Cookie#dot_domain if you need "." at the beginning.
+success
+Cookie#domain returns dot-less domain name now. Use Cookie#dot_domain if you need "." at the beginning.
+success
+rake aborted!
+SyntaxError: /Users/anzaiyuusuke/Desktop/rails/twitter-analytics-acquirer/lib/tasks/sns.rake:43: syntax error, unexpected keyword_end, expecting end-of-input
+/Users/anzaiyuusuke/Desktop/rails/twitter-analytics-acquirer/Rakefile:6:in `<top (required)>'
+/Users/anzaiyuusuke/.rbenv/versions/2.4.2/bin/bundle:23:in `load'
+/Users/anzaiyuusuke/.rbenv/versions/2.4.2/bin/bundle:23:in `<main>'
+(See full trace by running task with --trace)
+rake aborted!
+Don't know how to build task 'sns:script:script' (see --tasks)
+Did you mean?  script:script
+/Users/anzaiyuusuke/.rbenv/versions/2.4.2/bin/bundle:23:in `load'
+/Users/anzaiyuusuke/.rbenv/versions/2.4.2/bin/bundle:23:in `<main>'
+(See full trace by running task with --trace)
+Cookie#domain returns dot-less domain name now. Use Cookie#dot_domain if you need "." at the beginning.
+success
+Cookie#domain returns dot-less domain name now. Use Cookie#dot_domain if you need "." at the beginning.
+success
+Cookie#domain returns dot-less domain name now. Use Cookie#dot_domain if you need "." at the beginning.
+success
+rake aborted!
+Don't know how to build task 'sns:script:script' (see --tasks)
+Did you mean?  sns:script
+/Users/anzaiyuusuke/.rbenv/versions/2.4.2/bin/bundle:23:in `load'
+/Users/anzaiyuusuke/.rbenv/versions/2.4.2/bin/bundle:23:in `<main>'
+(See full trace by running task with --trace)
+rake aborted!
+Don't know how to build task 'sns:script:script' (see --tasks)
+/Users/anzaiyuusuke/.rbenv/versions/2.4.2/bin/bundle:23:in `load'
+/Users/anzaiyuusuke/.rbenv/versions/2.4.2/bin/bundle:23:in `<main>'
+(See full trace by running task with --trace)
+rake aborted!
+Don't know how to build task 'sns:script:script' (see --tasks)
+/Users/anzaiyuusuke/.rbenv/versions/2.4.2/bin/bundle:23:in `load'
+/Users/anzaiyuusuke/.rbenv/versions/2.4.2/bin/bundle:23:in `<main>'
+(See full trace by running task with --trace)
+rake aborted!
+Don't know how to build task 'sns:script:script' (see --tasks)
+/Users/anzaiyuusuke/.rbenv/versions/2.4.2/bin/bundle:23:in `load'
+/Users/anzaiyuusuke/.rbenv/versions/2.4.2/bin/bundle:23:in `<main>'
+(See full trace by running task with --trace)
+rake aborted!
+Don't know how to build task 'sns:script:script' (see --tasks)
+/Users/anzaiyuusuke/.rbenv/versions/2.4.2/bin/bundle:23:in `load'
+/Users/anzaiyuusuke/.rbenv/versions/2.4.2/bin/bundle:23:in `<main>'
+(See full trace by running task with --trace)
+Cookie#domain returns dot-less domain name now. Use Cookie#dot_domain if you need "." at the beginning.
+success
+Cookie#domain returns dot-less domain name now. Use Cookie#dot_domain if you need "." at the beginning.
+success
+Cookie#domain returns dot-less domain name now. Use Cookie#dot_domain if you need "." at the beginning.
+success
+Cookie#domain returns dot-less domain name now. Use Cookie#dot_domain if you need "." at the beginning.
+success
+Cookie#domain returns dot-less domain name now. Use Cookie#dot_domain if you need "." at the beginning.
+success
+Cookie#domain returns dot-less domain name now. Use Cookie#dot_domain if you need "." at the beginning.
+success
+Cookie#domain returns dot-less domain name now. Use Cookie#dot_domain if you need "." at the beginning.
+success
+Cookie#domain returns dot-less domain name now. Use Cookie#dot_domain if you need "." at the beginning.
+success
+Cookie#domain returns dot-less domain name now. Use Cookie#dot_domain if you need "." at the beginning.
+success
+Cookie#domain returns dot-less domain name now. Use Cookie#dot_domain if you need "." at the beginning.
+success
