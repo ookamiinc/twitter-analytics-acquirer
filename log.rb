68b0ec4b17a5fddd951f18d9dcae765b1a5980e3
Yusuke Anzai <qowgahoh@gmail.com>
2018-08-27 17:40:56 +0900

    Remove useless files

--- log.rb
+++ log.rb
@@ -86,3 +86,13 @@ Cookie#domain returns dot-less domain name now. Use Cookie#dot_domain if you nee
 success
 Cookie#domain returns dot-less domain name now. Use Cookie#dot_domain if you need "." at the beginning.
 success
+Cookie#domain returns dot-less domain name now. Use Cookie#dot_domain if you need "." at the beginning.
+rake aborted!
+Errno::ENOENT: No such file or directory @ unlink_internal - tweet_activity_metrics_handball_japans_20180730_20180828_ja.csv
+/Users/anzaiyuusuke/Desktop/rails/twitter-analytics-acquirer/lib/tasks/twitter_data.rake:39:in `delete'
+/Users/anzaiyuusuke/Desktop/rails/twitter-analytics-acquirer/lib/tasks/twitter_data.rake:39:in `block (2 levels) in <top (required)>'
+/Users/anzaiyuusuke/.rbenv/versions/2.4.2/bin/bundle:23:in `load'
+/Users/anzaiyuusuke/.rbenv/versions/2.4.2/bin/bundle:23:in `<main>'
+Tasks: TOP => twitter_data:output
+(See full trace by running task with --trace)
+success
