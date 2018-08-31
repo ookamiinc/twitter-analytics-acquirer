eae53e7421267d55e321181f41c284644d9b449a
Yusuke Anzai <qowgahoh@gmail.com>
2018-08-31 15:31:04 +0900

    Change script followed by changing method and valiable names

--- script.rb
+++ script.rb
@@ -5,22 +5,23 @@ require 'mechanize'
 require 'rails'
 require 'byebug'
 
-require './mechanize'
+require './google_data_sender'
 require './twitter_acquier'
 
 id_array = [ENV['TWITTER_ID']]
 
 id_array.each do |id|
 
-  pass, sp_url = ENV['TWITTER_PASS'], ENV['SPREADSHEET_URL'] if id == ENV['TWITTER_ID']
+  pass, spreadsheet_url = ENV['TWITTER_PASS'], ENV['SPREADSHEET_URL'] if id == ENV['TWITTER_ID']
 
   agent = Mechanize.new
-  agent.login_to_twitter(id, pass)
-  acquirer = TwitterAcquier.new
-  export_url, bundle_url = acquirer.sending_url(id)
-  csv = acquirer.obtain_csv(agent, export_url, bundle_url)
-  session = acquirer.session_to_google_auth
-  sp = session.spreadsheet_by_url(sp_url)
-  ws = sp.worksheet_by_title("test_volley")
-  acquirer.send_data_to_sp(csv, ws)
+  scraper = TwitterScraper.new
+  scraper.login_to_twitter(agent, id, pass)
+  export_url, bundle_url = scraper.set_scraping_url(id)
+  analytics_data = scraper.obtain_analytics_data(agent, export_url, bundle_url)
+  data_sender = GoogleDataSender.new
+  session = data_sender.session_to_google_auth
+  spreadsheet = session.spreadsheet_by_url(spreadsheet_url)
+  sheet = spreadsheet.worksheet_by_title("test_volley")
+  data_sender.send_data_to_spreadsheet(analytics_data, sheet)
 end
