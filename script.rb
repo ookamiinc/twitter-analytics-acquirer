eb40b3e68f3946b0a27fe8fb2118e2443faef00a
Yusuke Anzai <qowgahoh@gmail.com>
2018-09-03 13:32:21 +0900

    Refacot script followed by refacotirng other classes

--- script.rb
+++ script.rb
@@ -1,27 +1,13 @@
-require 'csv'
-require "googleauth"
-require "google_drive"
-require 'mechanize'
-require 'rails'
+require './twitter_analytics_client'
+require './google_sheet_client'
 require 'byebug'
 
-require './google_data_sender'
-require './twitter_scraper'
+twitter_user = ENV['TWITTER_ID']
+twitter_password = ENV['TWITTER_PASS']
+spreadsheet_url = ENV['SPREADSHEET_URL']
+analytics_client = TwitterAnalyticsClient.new(twitter_user, twitter_password)
+analytics_client.login
+csv = analytics_client.get_analytics_data
 
-id_array = [ENV['TWITTER_ID']]
-
-id_array.each do |id|
-
-  pass, spreadsheet_url = ENV['TWITTER_PASS'], ENV['SPREADSHEET_URL'] if id == ENV['TWITTER_ID']
-
-  agent = Mechanize.new
-  scraper = TwitterScraper.new
-  scraper.login_to_twitter(agent, id, pass)
-  export_url, bundle_url = scraper.set_scraping_url(id)
-  analytics_data = scraper.obtain_analytics_data(agent, export_url, bundle_url)
-  data_sender = GoogleDataSender.new
-  session = data_sender.session_to_google_auth
-  spreadsheet = session.spreadsheet_by_url(spreadsheet_url)
-  sheet = spreadsheet.worksheet_by_title("test_volley")
-  data_sender.send_data_to_spreadsheet(analytics_data, sheet)
-end
+sheet_client = GoogleSheetClient.new(spreadsheet_url)
+sheet_client.write_in_spreadsheet(csv, 'test_volley')
