6f693c27f61fb56866e9bc1ac25af174bc82c8de
Yusuke Anzai <qowgahoh@gmail.com>
2018-09-09 20:56:00 +0900

    Add environmental variables to worksheet_name

--- script.rb
+++ script.rb
@@ -5,9 +5,10 @@ require 'byebug'
 twitter_user = ENV['TWITTER_ID']
 twitter_password = ENV['TWITTER_PASS']
 spreadsheet_url = ENV['SPREADSHEET_URL']
+worksheet_name = ENV['WORKSHEET_NAME']
 analytics_client = TwitterAnalyticsClient.new(twitter_user, twitter_password)
 analytics_client.login
 csv = analytics_client.get_analytics_data
 
 sheet_client = GoogleSheetClient.new(spreadsheet_url)
-sheet_client.write_in_spreadsheet(csv, 'test_volley')
+sheet_client.write_in_spreadsheet(csv, worksheet_name)
