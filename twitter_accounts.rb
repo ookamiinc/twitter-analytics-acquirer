cbc95d4369904fda82fa66ae1ae6a32f7c9f8c70
Yusuke Anzai <qowgahoh@gmail.com>
2018-09-17 21:15:10 +0900

    Create DB and establish connection

---
+++ twitter_accounts.rb
@@ -0,0 +1,10 @@
+require 'active_record'
+require 'mysql2'
+
+ActiveRecord::Base.establish_connection(
+    "adapter" =>"mysql2",
+    "database" => "twitter_analytics_acquirer"
+)
+
+class TwitterAccounts < ActiveRecord::Base
+end
