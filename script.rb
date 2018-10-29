bb7272aee9dd2996297e34b8db62a2c08406a5c0
Yusuke Anzai <qowgahoh@gmail.com>
2018-10-29 20:37:28 +0900

    Add next step

--- script.rb
+++ script.rb
@@ -3,10 +3,12 @@
 require './twitter_analytics_client'
 require './google_sheet_client'
 require './database_client'
+require 'byebug'
 
 spreadsheet_url = ENV['SPREADSHEET_URL']
 
 DatabaseClient.all.each do |twitter_account|
+  next unless twitter_account["name"] == 'Player_twi'
   analytics_client = TwitterAnalyticsClient.new(twitter_account)
   csv = analytics_client.get_analytics_data_with_cookies
   csv ||= analytics_client.get_analytics_data_with_login
