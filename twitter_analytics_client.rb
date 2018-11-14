23765caa28aa4e20d9174a3128d93397417de986
Yusuke Anzai <qowgahoh@gmail.com>
2018-11-14 19:59:32 +0900

    Add process to get_cached_line_length

--- twitter_analytics_client.rb
+++ twitter_analytics_client.rb
@@ -19,32 +19,48 @@ class TwitterAnalyticsClient
     return unless @account['cookies']
 
     set_cookies(@account['cookies'])
-    get_analytics_data
+    get_analytics_data(start_date: variable_start_date , end_date: now)
   end
 
   def get_analytics_data_with_login
     login
-    csv = get_analytics_data
+    csv = get_analytics_data(start_date: variable_start_date , end_date: now)
     save_cookies if csv
     csv
   end
 
+  def get_cached_line_length
+    start_date = variable_boundary_date(former_period = 2)
+    end_date = variable_boundary_date(former_period = 1)
+    csv = get_analytics_data(start_date: start_date , end_date: end_date)
+    save_length(csv.length)
+  end
+
+  def save_length(length)
+    DatabaseClient.update('stored_csv_length', length, @account['id'])
+  end
+
   private
 
-  def start_date
-    @start_date ||= Time.parse('2018-10-01 00:00:00 +0900').to_i.to_s + '000'
+  def variable_start_date
+    return month_beggining if Time.now.strftime('%d').to_i < 11
+    variable_boundary_date
   end
 
-  def end_date
-    @end_date ||= Time.parse('2018-10-31 23:00:00 +0900').to_i.to_s + '000'
+  def variable_boundary_date(former_period = 0)
+    day_now = Time.now.strftime('%d').to_i
+    start_day = 5 * (day_now.div(5) - 1 - former_period)
+    month = Time.now.strftime('%m').to_i
+    @variable_boundary_date ||= Time.parse("#{month}/#{start_day}").to_i.to_s + '000'
   end
 
-  def export_url
-    "#{ANALYTICS_URI}/#{@account['name'].downcase}/tweets/export.json?start_time=#{start_date}&end_time=#{end_date}&lang=ja"
+  def month_beggining
+    month = Time.now.strftime('%m').to_i
+    @month_beggining ||= Time.parse("#{month}/1").to_i.to_s + '000'
   end
 
-  def bundle_url
-    "#{ANALYTICS_URI}/#{@account['name'].downcase}/tweets/bundle?start_time=#{start_date}&end_time=#{end_date}&lang=ja"
+  def now
+    @now ||= Time.now.utc.to_i.to_s + '999'
   end
 
   def set_cookies(cookies_yaml)
@@ -71,16 +87,18 @@ class TwitterAnalyticsClient
     form.submit
   end
 
-  def get_analytics_data
+  def get_analytics_data(start_date: start_date , end_date: end_date)
     return if @agent.nil?
 
     for i in 1..50 do
       #puts 'post送る前!!============================'
       #puts ''
+      export_url = make_export_url(start_date, end_date)
       pos = @agent.post(export_url)
       #@logger.debug(pos)
       #puts 'post送った後============================'
       #puts ''
+      bundle_url = make_bundle_url(start_date, end_date)
       res = @agent.get(bundle_url)
       #@logger.debug(res)
       #puts 'get送った後============================'
@@ -88,8 +106,17 @@ class TwitterAnalyticsClient
       puts 'nil!!' if res.body.empty?
       sleep(5)
       puts i unless res.body.empty?
+      puts 'タイトルだけ！' if res.body.count(',') == 39
       break unless res.body.empty?
     end
     res.body.force_encoding('utf-8')
+  end
+
+  def make_export_url(start_date, end_date)
+    "#{ANALYTICS_URI}/#{@account['name'].downcase}/tweets/export.json?start_time=#{start_date}&end_time=#{end_date}&lang=ja"
+  end
+
+  def make_bundle_url(start_date, end_date)
+    "#{ANALYTICS_URI}/#{@account['name'].downcase}/tweets/bundle?start_time=#{start_date}&end_time=#{end_date}&lang=ja"
   end
 end
