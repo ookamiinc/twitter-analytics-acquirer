9ed7434307b03752d3f67402db373fe8b075446b
Yusuke Anzai <qowgahoh@gmail.com>
2018-11-14 19:55:35 +0900

    Add process to store csv line length

--- script.rb
+++ script.rb
@@ -13,4 +13,5 @@ DatabaseClient.all.each do |twitter_account|
   csv ||= analytics_client.get_analytics_data_with_login
   sheet_client = GoogleSheetClient.new(spreadsheet_url)
   sheet_client.write_in_spreadsheet(csv, twitter_account['worksheet_name'])
+  analytics_client.get_cached_line_length #if Time.now.strftime('%d').to_i.div(5) == 1 && Time.now.strftime('%d').to_i.div(5) >= 11
 end
