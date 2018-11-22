df66b3865341c6a72ffd43d8ebf69c1273a510b7
Yusuke Anzai <qowgahoh@gmail.com>
2018-11-22 19:55:38 +0900

    Add rescue process to avoid SQL lost connection error

--- database_client.rb
+++ database_client.rb
@@ -14,6 +14,14 @@ class DatabaseClient
 
   def self.update(column, value, id)
     escaped_value = @@client.escape(value.to_s)
-    @@client.query("UPDATE `twitter_accounts` SET #{column} = '#{escaped_value}' WHERE id = #{id}")
+    begin
+      @@client.query("UPDATE `twitter_accounts` SET #{column} = '#{escaped_value}' WHERE id = #{id}")
+    rescue
+      @@client = Mysql2::Client.new(host: ENV['HOST'],
+                                    username: ENV['DB_USERNAME'],
+                                    password: ENV['DB_PASSWORD'],
+                                    database: ENV['DATABASE'])
+      @@client.query("UPDATE `twitter_accounts` SET #{column} = '#{escaped_value}' WHERE id = #{id}")
+    end
   end
 end
