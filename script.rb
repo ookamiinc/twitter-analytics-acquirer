e6169a06ca361279336739e2c9e25452c093d3b1
Yusuke Anzai <qowgahoh@gmail.com>
2019-01-20 12:00:11 +0900

    Remove player_twi account from target accounts

--- script.rb
+++ script.rb
@@ -8,6 +8,9 @@ spreadsheet_url = ENV['SPREADSHEET_URL']
 
 DatabaseClient.all.each do |twitter_account|
   next if twitter_account['name'] == 'Playerapp_cbsk'
+  # I remove Player_twi because this account has too much data.
+  # But I want to save cookies data, so deal with it by adding next process.
+  next if twitter_account['name'] == 'Player_twi'
   analytics_client = TwitterAnalyticsClient.new(twitter_account)
   csv = analytics_client.get_analytics_data_with_cookies
   csv ||= analytics_client.get_analytics_data_with_login
